29ffd609106321619abfd8cb5de6da8da0648008
lpopczyk <107108231+lpopczyk@users.noreply.github.com>
2022-06-29 16:44:24 +0200

    Update pageTemplate.js

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -1,80 +1,24 @@
 import Layout from "../components/Layout"
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { Link, graphql } from "gatsby"
+import Characters from "../components/character"
 import {
     containercharacter,
     tilte,
     containerpagination,
     pagination__pgnumber,
 } from '../scss/pageTemplate.module.scss'
-import {
-    tiltepage,
-    searchContainer,
-    searchBar,
-    resultcontainer,
-    card,
-    cardimage,
-    charname,
-    cardinfo,
-    more,
-    charninfotitre,
-    charninfo,
-} from '../scss/searchCharacter.module.scss'
 
 const PageTemplate = ({ data, pageContext }) => {
 
-    //pagination
-    const [allcharacters, setAllcharacters] = useState(
+    //total characters
+    const [characters] = useState(
         data.ram.characters.results
     )
-    const [totalpage, setTotalpage] = useState(
+    //total pages
+    const [totalpage] = useState(
         data.ram.characters.info.pages
     )
-
-    const [searchText] = useState('')
-
-    const [currentPage, setCurrentPage] = useState(pageContext.page)
-    const [loading, setLoading] = useState(false)
-    const [searchMode, setSearchMode] = useState(false)
-
-    const updateCharacters = () => {
-        setSearchMode(true)
-        if (searchText === "") {
-            return
-        }
-        setLoading(true)
-    }
-
-    useEffect(() => {
-        updateCharacters()
-    }, [currentPage])
-
-    //search
-    const [state, setState] = useState({
-        filteredData: [],
-        query: [searchText],
-    })
-
-    const handleInputChange = event => {
-        const query = event.target.value
-
-        const characters = data.ram.characters.results || []
-
-        const filteredData = characters.filter(data => {
-            const { name } = data
-            return (
-                name.toLowerCase().includes(query.toLowerCase())
-            )
-        })
-
-        setState({
-            query,
-            filteredData,
-        })
-}
-    const { filteredData, query } = state
-    const hasSearchResults = filteredData && query !== [searchText]
-    const characters = hasSearchResults ? filteredData : [allcharacters]
 
     return (
         //print card character, create pagination
@@ -82,73 +26,22 @@ const PageTemplate = ({ data, pageContext }) => {
             <div>
                 <div className={containercharacter}> 
                     <h1 className={tilte}>Character List</h1>
-                    <div className={searchContainer}>
-                    <input
-                        className={searchBar}
-                        type="text"
-                        placeholder="Search"
-                        onChange={handleInputChange}
-                    />
-                    </div>
-            </div>
-            <div className={resultcontainer}>
-                {characters.map(({id, image, name, gender, species}) => {
-                return <span key={id}>
-                        <div className={card}>
-                            <div>
-                                <img className={cardimage} src={image} alt="" />
-                            </div>
-                            <h1 className={charname}>{name}</h1>
-                            <table className={cardinfo}>
-                                <tr>
-                                    <td className={charninfotitre}>
-                                        Gender
-                                    </td>
-                                    <td className={charninfo}>
-                                        {gender}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className={charninfotitre}>
-                                        Species
-                                    </td>
-                                    <td className={charninfo}>
-                                        {species}
-                                    </td>
-                                </tr>
-                            </table>
-                            <Link 
-                            className={more}
-                            to={`/character/${id}`}
-                        >→</Link>
-                        </div>
-                    </span>   
-                })}
-            </div>
-                {loading ? (
-                    ""
-                ) : characters.length > 0 ? (
-                    <div className={containerpagination}>
+                    <Characters characters={characters} />
+                    <br></br><br></br><br></br>
+                </div>
+                <div className={containerpagination}>
                         {[...Array(totalpage)].map((_, pageNo) => (
                             <Link
+                                className={pagination__pgnumber}  
                                 to={`/page/${pageNo + 1}`}
-                                key={pageNo}
-                                className={pagination__pgnumber}
-                                onClick={e => {
-                                    if (searchText !== "" && searchMode) {
-                                        e.preventDefault()
-                                        setCurrentPage(pageNo + 1)
-                                    }
-                                }}
+                                key={pageNo}  
                             >
-                                <div>{pageNo + 1}</div>
+                                <div>
+                                    {pageNo + 1}
+                                </div>
                             </Link>
                         ))}
                     </div>
-
-                ) : (
-                    ""
-                )}
             </div>
         </Layout>
     )
@@ -165,18 +58,12 @@ export const query = graphql`
           pages
         }
         results {
+          id
           name
           gender
-          id
           image
-          location {
-            name
-          }
-          origin {
-            name
-          }
           species
-          status
+          status  
         }
       }
     }
